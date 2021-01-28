1dbffc96201a7fd78d5d83fae0c578a12e324f3b
Leng <jerome.leng@etud.univ-angers.fr>
2021-01-28 14:38:54 +0100

    Création cryptage

---
+++ src/Cryptage.java
@@ -0,0 +1,45 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class Cryptage {
+	//Première étape
+	public static ArrayList<BigInteger> convert_ascii(String s){
+		ArrayList<BigInteger> asciiList = new ArrayList<BigInteger>();
+		char[] characters = s.toCharArray();
+		for(char c : characters) {
+			asciiList.add(BigInteger.valueOf((long) c));
+		}
+
+		return asciiList;
+	}
+	
+	//Seconde étape
+	public static ArrayList<BigInteger> chiffrement(ArrayList<BigInteger> asciiList, Key kpublic){
+		ArrayList<BigInteger> chiffreList = new ArrayList<BigInteger>();
+		for(BigInteger c: asciiList) {
+			BigInteger Si = c.modPow(kpublic.getY(), kpublic.getN());
+			chiffreList.add(Si);
+		}
+		return chiffreList;
+	}
+	
+	public static ArrayList<BigInteger> dechiffrement(ArrayList<BigInteger> asciiList, Key kprivate){
+		ArrayList<BigInteger> chiffreList = new ArrayList<BigInteger>();
+		for(BigInteger c: asciiList) {
+			BigInteger Si = c.modPow(kprivate.getY(), kprivate.getN());
+			chiffreList.add(Si);
+		}
+		return chiffreList;
+	}
+	
+	public static String ascii_to_string(ArrayList<BigInteger> asciiList ){
+		String s ="";
+		for(BigInteger ascii : asciiList) {
+			int n = ascii.intValueExact();
+			char ch = (char) n;
+			s+=ch;
+		}
+
+		return s;
+	}
+}
--- src/main.java
+++ src/main.java
@@ -3,47 +3,6 @@ import java.util.ArrayList;
 
 public class main {
 	
-	//Première étape
-	public static ArrayList<BigInteger> convert_ascii(String s){
-		ArrayList<BigInteger> asciiList = new ArrayList<BigInteger>();
-		char[] characters = s.toCharArray();
-		for(char c : characters) {
-			asciiList.add(BigInteger.valueOf((long) c));
-		}
-
-		return asciiList;
-	}
-	
-	//Seconde étape
-	public static ArrayList<BigInteger> chiffrement(ArrayList<BigInteger> asciiList, Key kpublic){
-		ArrayList<BigInteger> chiffreList = new ArrayList<BigInteger>();
-		for(BigInteger c: asciiList) {
-			BigInteger Si = c.modPow(kpublic.getY(), kpublic.getN());
-			chiffreList.add(Si);
-		}
-		return chiffreList;
-	}
-	
-	public static ArrayList<BigInteger> dechiffrement(ArrayList<BigInteger> asciiList, Key kprivate){
-		ArrayList<BigInteger> chiffreList = new ArrayList<BigInteger>();
-		for(BigInteger c: asciiList) {
-			BigInteger Si = c.modPow(kprivate.getY(), kprivate.getN());
-			chiffreList.add(Si);
-		}
-		return chiffreList;
-	}
-	
-	public static String ascii_to_string(ArrayList<BigInteger> asciiList ){
-		String s ="";
-		char[] characters = s.toCharArray();
-		for(BigInteger ascii : asciiList) {
-			int n = ascii.intValueExact();
-			char ch = (char) n;
-			s+=ch;
-		}
-
-		return s;
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -60,17 +19,12 @@ public class main {
 		PrivateKey pk = new PrivateKey(clePublic.getY(),p.get_m(),clePublic.getN());
 		pk.CreationKey();
 		Key clePrive = pk.getPKey();
-
-//		char car = 'B';
-//		int ascii = (int) car;
-//		p.convert_ascii("Bonjour !");
-
 		
-		ArrayList<BigInteger> phrase = convert_ascii("Bonjour !");
+		ArrayList<BigInteger> phrase = Cryptage.convert_ascii("Bonjour !");
 		
 		System.out.println("-----CHIFFREMENT-----");
 		
-		ArrayList<BigInteger> texte_chiffre =  chiffrement(phrase,clePublic);
+		ArrayList<BigInteger> texte_chiffre =  Cryptage.chiffrement(phrase,clePublic);
 		
 		for ( BigInteger t : texte_chiffre) {
 			System.out.println(t.longValueExact());
@@ -91,16 +45,14 @@ public class main {
 //		dechiffrement.add(BigInteger.valueOf(1858));
 		
 		
-		ArrayList<BigInteger> texte_asciichiffre =  dechiffrement(texte_chiffre,clePrive);
+		ArrayList<BigInteger> texte_asciichiffre =  Cryptage.dechiffrement(texte_chiffre,clePrive);
 		
 		for ( BigInteger t : texte_asciichiffre) {
 			System.out.println(t.longValueExact());
 		}
 		
-		System.out.println(ascii_to_string(texte_asciichiffre));
+		System.out.println(Cryptage.ascii_to_string(texte_asciichiffre));
 		
 	}
 	
-
-
 }
